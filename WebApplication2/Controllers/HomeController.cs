2d327f2d864103a28b1b04ec4b38c821b8a84327
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-08-14 11:48:04 +0300

    Lisätty yksinkertainen ASP.NET MVC -sovellus versionhallintaan.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TallennaEnnatys(int? id)
+        {
+            if (id.HasValue)
+            {
+                int arvaustenmäärä = id.Value;
+                // TODO: tallennus tietokantaan
+            }
+
+            var ok = new { success = true };
+            return Json(ok, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
